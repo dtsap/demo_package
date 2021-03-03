d5a6417794d9b048baf53ce93ed9b982e52cae69
Dimitrios Tsapnidis <dimitrios.tsapnidis@gmail.com>
2021-03-03 22:59:00 +0200

    Change the version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="demo_package", # Replace with your own username
-    version="0.0.3",
+    version="0.0.4",
     author="Dimitrios Tsapnidis",
     author_email="dimitrios.tsapndis@gmail.com",
     description="A demo package",
